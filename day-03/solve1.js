f9aeb27f337f5864c8526b8c6692520831ecd2bd
Ron Snyder <ronalddsnyder@gmail.com>
2023-12-28 15:46:34 -0500

    day-03 solution 1

--- day-03/solve1.js
+++ day-03/solve1.js
@@ -6,17 +6,68 @@ const lines = getInput(input);
 
 const specialChars = getSpecialChars(lines);
 
-const getNumbersOnLine = (line, index) => {
-    const re = /\d+/g;
-    return { data: re.exec(line), line: index };
+const isSymbol = (char) => {
+    return specialChars.includes(char);
 };
 
-const numbers = lines.map(getNumbersOnLine);
+const numbers = lines.reduce((acc, curr, index) => {
+    const re = /\d+/g;
+    let match;
+
+    while ((match = re.exec(curr)) !== null) {
+        acc.push({ data: match, line: index });
+    }
+
+    return acc;
+}, []);
+
+const lineCharArray = lines.map((line) => line.split(""));
 
 const validNumbers = numbers.filter((number) => {
-    const line = number.line;
+    const lineIndex = number.line;
     const data = number.data;
-    const isValid = true;
+    let isValid = false;
+    const currentLine = lineCharArray[lineIndex];
+    const previousLine = lineIndex !== 0 ? lineCharArray[lineIndex - 1] : [];
+
+    const nextLine =
+        lineIndex === lineCharArray.length - 1
+            ? []
+            : lineCharArray[lineIndex + 1];
+
+    const numberStartIndex = data.index === 0 ? 0 : data.index - 1;
+
+    const numberEndIndex =
+        data[0].length + data.index >= currentLine.length
+            ? currentLine.length
+            : data[0].length + data.index;
+
+    if (isSymbol(currentLine[numberStartIndex])) {
+        isValid = true;
+    }
+    if (isSymbol(currentLine[numberEndIndex])) {
+        isValid = true;
+    }
+    if (previousLine.length > 0) {
+        for (let i = numberStartIndex; i <= numberEndIndex; i++) {
+            if (isSymbol(previousLine[i])) {
+                isValid = true;
+            }
+        }
+    }
+    if (nextLine.length > 0) {
+        for (let i = numberStartIndex; i <= numberEndIndex; i++) {
+            if (isSymbol(nextLine[i])) {
+                isValid = true;
+            }
+        }
+    }
+
+    return isValid;
 });
 
-return validNumbers;
+const solution = validNumbers.reduce((acc, curr) => {
+    return (acc += parseInt(curr.data[0]));
+}, 0);
+
+console.log(solution);
